e7a3d2981798ade535d89d19192f568cd8bd6be7
jwolf <jwolf@student.wethinkcode.co.za>
2020-08-07 14:59:46 +0200

    wtc-lms save

--- hangman.py
+++ hangman.py
@@ -1,19 +1,32 @@
 #TIP: use random.randint to get a random word from the list
 import random
+from os import read
 
 
 def read_file(file_name):
     """
     TODO: Step 1 - open file and read lines as words
     """
-    return []
+    #dest = "/home/wtc/problems/submission_001-hangman/short_words.txt"
+    my_file =open(file_name,"r")
+    words = my_file.readlines()   
+    
+    return words
 
 
 def select_random_word(words):
     """
     TODO: Step 2 - select random word from list of file
     """
-    return words[0]
+    rand_word  =random.randint(0,len(words)-1)
+    word = words[rand_word]
+
+    missing_letter = random.randint(0,len(word)-2)
+    letter = list(word)
+    letter[missing_letter] = '_'
+    print("Guess the word: " + ''.join(letter))
+
+    return word
 
 
 
@@ -21,7 +34,9 @@ def get_user_input():
     """
     TODO: Step 3 - get user input for answer
     """
-    return 'TODO'
+    guess = input("Guess the missing letter: ")
+     
+    return guess
 
 
 def run_game(file_name):
@@ -32,6 +47,7 @@ def run_game(file_name):
     word = select_random_word(words)
     answer = get_user_input()
     print('The word was: '+word)
+    #print(words)
 
 
 if __name__ == "__main__":
